cdfe636ee5a39cefce226da177f27b61f459abbf
JulianoQueiroz <juliano.invest@outlook.com>
2020-12-14 11:57:28 -0300

    Update index.js

--- euweb/src/components/Header/index.js
+++ euweb/src/components/Header/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import '../Header/styles.css';
 
-const Header = () => <header id="main-header">From Facebook </header>;
+const Header = () => <header id="main-header">Find me on facebook </header>;
 
 export default Header;
 
